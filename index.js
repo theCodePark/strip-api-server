898a3c4802227db307e94add49ded520515a13a8
Ashwin Talwalkar <atalwalkar719@gmail.com>
2023-05-29 15:29:57 -0700

    fixed bug try 12232

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require("dotenv").config()
 const express = require("express")
 const app = express()
-const stripe = require("stripe")(process.env.STRIPE_SECRET_LIVE)
+const stripe = require("stripe")(`${process.env.STRIPE_SECRET_LIVE}`)
 const bodyParser = require("body-parser")
 const cors = require("cors")
 
