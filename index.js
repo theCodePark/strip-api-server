fc1b5c50bd34fdd8d2dc4ec796ae26e99b31fa77
Ashwin Talwalkar <atalwalkar719@gmail.com>
2023-05-29 11:50:21 -0700

    testing actual server

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.post("/payment", cors(), async (req, res) => {
 	} catch (error) {
 		console.log("Error", error)
 		res.json({
-			message: "Payment failed: " + error,
+			message: "Payment failed: " + error + "key: " + process.env.STRIPE_SECRET_LIVE,
 			success: false
 		})
 	}
